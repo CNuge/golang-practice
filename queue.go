500f3f018b157e3cd7d486ceff51f31193b001cc
WATTBA <nugentc@uoguelph.ca>
2018-02-19 09:10:25 -0500

    queue completed

--- queue.go
+++ queue.go
@@ -10,3 +10,51 @@ code one with these operators:
 
 
 */
+
+package main
+
+import(
+	"fmt"
+	)
+
+
+type queue struct {
+	ord []int
+}
+
+//representation of the queue, just the order
+func (q queue) String() string {
+	return fmt.Sprintf("%v", q.ord)
+}
+
+// add an integer to the back of the list
+func (q *queue) Add(item int) []int {
+	q.ord = append(q.ord, item)
+	return q.ord
+}
+
+// remove
+func (q *queue) Remove() int {
+	output := q.ord[0]
+	q.ord = q.ord[1:]
+	return output
+}
+
+// look at the first position in the queue
+func (q *queue) Peek() int {
+	return q.ord[0]
+}
+
+
+func main(){
+	q := queue{}
+	q.Add(7)
+	q.Add(8)
+	q.Add(9)
+	fmt.Println(q)
+	fmt.Println(q.Peek())
+	fmt.Println(q)
+	fmt.Println(q.Remove())
+	fmt.Println(q.Peek())
+	fmt.Println(q)
+}
