1fc42c2d086748697f7739976641ac037a7068b4
WATTBA <nugentc@uoguelph.ca>
2018-02-20 13:08:35 -0500

    initial go

---
+++ binf_problems/test.go
@@ -0,0 +1,9 @@
+package main
+
+import("fmt")
+
+func main(){
+	for i:=1 ; i<25; i = i+2{
+		fmt.Println(i)
+	}
+}
--- binf_problems/translation.go
+++ binf_problems/translation.go
@@ -7,26 +7,25 @@ package main
 
 import(
 	"fmt"
-	"string"
 )
 
 // the translation map RNA -> AA
 var trans_map = map[string]string{"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
-								"UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
-								"UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
-								"UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
-								"CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
-								"CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
-								"CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
-								"CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
-								"AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
-								"ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
-								"AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
-								"AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
-								"GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
-								"GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
-								"GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
-								"GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
+									"UCU":"S", "UCC":"S", "UCA":"S", "UCG":"S",
+									"UAU":"Y", "UAC":"Y", "UAA":"STOP", "UAG":"STOP",
+									"UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+									"CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
+									"CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
+									"CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
+									"CGU":"R", "CGC":"R", "CGA":"R", "CGG":"R",
+									"AUU":"I", "AUC":"I", "AUA":"I", "AUG":"M",
+									"ACU":"T", "ACC":"T", "ACA":"T", "ACG":"T",
+									"AAU":"N", "AAC":"N", "AAA":"K", "AAG":"K",
+									"AGU":"S", "AGC":"S", "AGA":"R", "AGG":"R",
+									"GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
+									"GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
+									"GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
+									"GGU":"G", "GGC":"G", "GGA":"G", "GGG":"G",}
 
 // a type to represent the transcript in both formats
 type transcript struct{
@@ -34,14 +33,47 @@ type transcript struct{
 	protein string
 }
 
+// find the First AA after the 
+func (tr *transcript) Frame() int {
+	// start scan of rna with [0:3] if not AUG, add one to index
+	for index := 0 ; index < len(tr.rna); index++ {
+		if tr.rna[index:index + 3] == "AUG" {
+			start_pos := index + 3
+			return start_pos
+		}
+	}
+	return len(tr.rna)
+}
+
 // start with an initial bp scan to find "AUG"
-func (tr *transcript) Translate(){
-	index := 0 // start scan of rna with [0:4] if not AUG, add one to index
-	
+func (tr *transcript) Translate() string {
+	start := tr.Frame() // find the first AA's start position
+	fmt.Printf("Start position: %v\n", start)
+	for pos := start ; pos < len(tr.rna) -3 ; pos = pos + 3 {
+		aa := trans_map[tr.rna[pos: pos+3]]
+		if aa == "STOP" {
+			break
+		} else {
+			tr.protein = fmt.Sprintf("%v%v", tr.protein, aa )
+		}
+	}
+	return tr.protein
 }
 
 
 
 func main(){
+	transcript1 := transcript{ rna: "AAAUAUGGCCGCAGAGUAGGGGGG" }
+	transcript2 := transcript{ rna:  "AAUGUUUUUCCUAGCAAAAUGAAAA"}
+	transcript1.Translate()
+	fmt.Println(transcript1.protein)
+	transcript2.Translate()
+	fmt.Println(transcript2.protein)
+
+//errors to address:
+	// doesn't handle stop codons at the end of the string (this causes the error: tr.rna[pos: pos+3] )
+	// need to handle overflow (no stop codon)
+	// need to handle no start codon
+	// need to have a Stringer() method for 
 
 }
