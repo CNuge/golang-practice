97e2b8d45976fc54df2f15385d774c15806e414d
WATTBA <nugentc@uoguelph.ca>
2018-02-22 06:36:18 -0500

    string permutation

--- string_permutation.go
+++ string_permutation.go
@@ -2,5 +2,41 @@
 given two strings, see if one is a permuation of the other
 */
 
+package main
+
+import ( 
+	"fmt"
+	"strings"
+	"reflect"
+	"sort")
+
+/*
+ how to sort an array in standard library:
+	x := []string{"B","C","A"}
+	sort.Strings(x)
+	fmt.Println(x)
+sort:
+https://golang.org/pkg/sort/ 
+*/
+
 
 // sort and test equality
+func StrEq(str1, str2 string) bool {
+	s_str1 := strings.Split(str1, "")
+	s_str2 := strings.Split(str2, "")
+
+	sort.Strings(s_str1)
+	sort.Strings(s_str2)
+
+	return reflect.DeepEqual(s_str1, s_str2)
+}
+
+func main(){
+	str1 := "CAM NUGE"
+	str2 := "MAC NUGE"
+	str3 := "GREG NUGE"
+
+	fmt.Println(StrEq(str1, str2))
+	fmt.Println(StrEq(str1, str3))
+
+}
