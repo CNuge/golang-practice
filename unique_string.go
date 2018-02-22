fbf02b08bf499b9459052cb6cfc2aa58a5f09f1f
WATTBA <nugentc@uoguelph.ca>
2018-02-22 06:56:12 -0500

    unique string

--- unique_string.go
+++ unique_string.go
@@ -1,4 +1,34 @@
 /*
 write an algorithm to determine is a string has all unique characters
-- what if you can't use additional data structures?
 */
+
+package main
+
+import (
+	"fmt"
+	)
+
+// make a map and check if next character is in the map
+func IsUnique(str string) bool {
+	seen := make(map[rune]bool)
+	for _, i := range str {
+		if seen[i] == true {
+			return false
+		} else {
+			seen[i] = true
+		}
+	}
+	return true
+}
+
+
+
+
+func main(){
+	str1 := "CAMERON"
+	str2 := "SAMANTHA"
+
+	fmt.Println(IsUnique(str1))
+	fmt.Println(IsUnique(str2))
+
+}
