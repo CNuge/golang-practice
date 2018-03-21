db953f28b869a5c5eb1bcfc0d906771c1ac21803
WATTBA <nugentc@uoguelph.ca>
2018-03-21 07:14:05 -0400

    fmt

--- bioinformatics/homology.go
+++ bioinformatics/homology.go
@@ -1,23 +1,21 @@
 package main
 
-
-
-import(
+import (
 	"fmt"
 	"strings"
-	)
+)
 
 //"os"
 
 /*
-By counting the number of differences between two homologous DNA 
-strands taken from different genomes with a common ancestor, 
-we get a measure of the minimum number of point mutations that 
+By counting the number of differences between two homologous DNA
+strands taken from different genomes with a common ancestor,
+we get a measure of the minimum number of point mutations that
 could have occurred on the evolutionary path between the two strands.
 
 This is called the 'Hamming distance'.
 
-It is found by comparing two DNA strands and counting how many 
+It is found by comparing two DNA strands and counting how many
 of the nucleotides are different from their equivalent in the other string.
 
 
@@ -34,17 +32,16 @@ identity = x / y
 func homology_string(seq1, seq2 string) []string {
 	hom_str := []string{}
 
-	for i:= 0; i < len(seq1) ; i++ {
-		if seq1[i] == seq2[i]{
+	for i := 0; i < len(seq1); i++ {
+		if seq1[i] == seq2[i] {
 			hom_str = append(hom_str, " ")
 		} else {
-			hom_str = append(hom_str, "^")			
+			hom_str = append(hom_str, "^")
 		}
 	}
 
 	return hom_str
 }
-
 
 func homology_count(homo_string []string) int {
 	matches := 0
@@ -56,31 +53,22 @@ func homology_count(homo_string []string) int {
 	return matches
 }
 
-func main(){
+func main() {
 	// could turn this into an input receiving program
 	//seq1 := os.Args[1]
 	//seq2 := os.Args[2]
 	seq1 := "GAGCCTACTAACGGGAT"
 	seq2 := "CATCGTAATGACGGCCT"
 
-
 	homo_string := homology_string(seq1, seq2)
 
-	homo_num := homology_count(homo_string) 
+	homo_num := homology_count(homo_string)
 
 	fmt.Println(seq1)
 	fmt.Println(seq2)
 	fmt.Println(strings.Join(homo_string, ""))
-	fmt.Printf("number of matches: %v / %v \n", homo_num, len(homo_string) )
-	
+	fmt.Printf("number of matches: %v / %v \n", homo_num, len(homo_string))
 
-	fmt.Printf("%.2f%%  identity\n", float64(homo_num)/float64(len(homo_string)) *100 )
+	fmt.Printf("%.2f%%  identity\n", float64(homo_num)/float64(len(homo_string))*100)
 
 }
-
-
-
-
-
-
-
