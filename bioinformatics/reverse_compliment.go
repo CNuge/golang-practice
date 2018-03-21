f0db749a740833dc8b5b6f6412e3b9609e4abf8c
WATTBA <nugentc@uoguelph.ca>
2018-03-21 07:14:27 -0400

    fmt

--- bioinformatics/reverse_compliment.go
+++ bioinformatics/reverse_compliment.go
@@ -8,32 +8,31 @@ print it out 5'->3'
 
 package main
 
-import(
+import (
 	"fmt"
 	"strings"
 )
 
-var comp_map = map[string] string {
-			"A" : "T",
-			"T" : "A",
-			"G" : "C",
-			"C" : "G", }
+var comp_map = map[string]string{
+	"A": "T",
+	"T": "A",
+	"G": "C",
+	"C": "G"}
 
-// take a string of DNA as input, split it to an array and then 
-// iterate over the slice in reverse order, getting the complimentary bp 
+// take a string of DNA as input, split it to an array and then
+// iterate over the slice in reverse order, getting the complimentary bp
 // from the map
 
 func rev_comp(dna_string string) string {
 	original := strings.Split(dna_string, "")
-	rev_comp := [] string {}
-	for i := len(original) - 1 ; i >= 0 ; i-- {
+	rev_comp := []string{}
+	for i := len(original) - 1; i >= 0; i-- {
 		rev_comp = append(rev_comp, comp_map[original[i]])
 	}
 	return strings.Join(rev_comp, "")
 }
 
-
-func main(){
+func main() {
 	test1 := "AAAAACGCGTATATAT"
 	test2 := "GCGCGCGCGCGCGCGCGCAAATATATATGGCGGTTTTTTA"
 
@@ -46,5 +45,3 @@ func main(){
 	fmt.Println(comp_map["A"])
 
 }
-
-
