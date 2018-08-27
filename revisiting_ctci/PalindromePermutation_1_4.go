e358a434c1bd4dce94ae4a224a7c2130d0991897
CNuge <nugentc@uoguelph.ca>
2018-08-27 07:29:16 -0400

    getting other new problems ready

---
+++ revisiting_ctci/PalindromePermutation_1_4.go
@@ -0,0 +1,9 @@
+/*
+
+given a string check if it is a permutation of a palindrome
+
+i.e.
+
+rcarace - is a permutation of racecar, which is a palindrome
+
+*/
---
+++ revisiting_ctci/StringCompression_1_6.go
@@ -0,0 +1,10 @@
+
+/*
+take a string and compress it into repeated digit counts:
+
+AAAATTTTTGGG
+A4T5G3
+
+if the new string is longer than the original, then return the original.
+
+*/
