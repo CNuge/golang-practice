fb52bbfcca5929655b50c3d6eabeaa67335fd861
CNuge <nugentc@uoguelph.ca>
2018-08-28 06:48:02 -0400

    oneaway cleanup

--- revisiting_ctci/OneAway_1_5.go
+++ revisiting_ctci/OneAway_1_5.go
@@ -5,13 +5,11 @@ change (insertion, deletion, replacement) away from one another
 
 */
 
-
 package main
 
 import (
 	"fmt"
 )
-
 
 // take two strings and compare them to one another to see if they are one
 // insertion, deletion or replacement away from one another
@@ -19,11 +17,11 @@ func OneAway(s1, s2 string) bool {
 	diff := 0
 	long := ""
 	short := ""
-	
+
 	// check for replacement first, only valid if same length
-	if len(s1) == len(s2){
-		for i:=0 ; i < len(s1) ; i++ {
-			if s1[i] != s2[i]{
+	if len(s1) == len(s2) {
+		for i := 0; i < len(s1); i++ {
+			if s1[i] != s2[i] {
 				diff++
 			}
 			if diff > 1 {
@@ -31,8 +29,8 @@ func OneAway(s1, s2 string) bool {
 			}
 		}
 
-	// if they aren't the same size only one indel is allowed
-	// check to see if they are one different in length, determine the larger
+		// if they aren't the same size only one indel is allowed
+		// check to see if they are one different in length, determine the larger
 	} else if (len(s1) - len(s2)) == 1 {
 		long = s1
 		short = s2
@@ -44,7 +42,7 @@ func OneAway(s1, s2 string) bool {
 	}
 
 	offset := 0
-	for i := 0  ; i < len(short); i++ {
+	for i := 0; i < len(short); i++ {
 		li := i + offset
 		if short[i] != long[li] {
 			offset++
@@ -58,28 +56,22 @@ func OneAway(s1, s2 string) bool {
 	return true
 }
 
-
-
-
 func main() {
 
 	t1 := "TATTA"
 	t2 := "TACTTA"
-	fmt.Printf("%v\t%v\t%v\n", t1, t2, OneAway(t1,t2))
+	fmt.Printf("%v\t%v\t%v\n", t1, t2, OneAway(t1, t2))
 
 	t3 := "CGCGCG"
 	t4 := "CGCTCG"
-	fmt.Printf("%v\t%v\t%v\n", t3, t4, OneAway(t3,t4))
-
+	fmt.Printf("%v\t%v\t%v\n", t3, t4, OneAway(t3, t4))
 
 	t5 := "ATGCATGC"
 	t6 := "ATGCTTATGC"
-	fmt.Printf("%v\t%v\t%v\n", t5, t6, OneAway(t5,t6))
-
+	fmt.Printf("%v\t%v\t%v\n", t5, t6, OneAway(t5, t6))
 
 	t7 := "GGGGTAC"
 	t8 := "GGCCTAC"
-	fmt.Printf("%v\t%v\t%v\n", t7, t8, OneAway(t7,t8))
-
+	fmt.Printf("%v\t%v\t%v\n", t7, t8, OneAway(t7, t8))
 
 }
--- revisiting_ctci/StringCompression_1_6.go
+++ revisiting_ctci/StringCompression_1_6.go
@@ -8,3 +8,17 @@ A4T5G3
 if the new string is longer than the original, then return the original.
 
 */
+
+package main
+
+import (
+	"fmt"
+)
+
+
+
+
+func main() {
+
+
+}
