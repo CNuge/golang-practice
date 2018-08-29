1feabbbed397d4e4c3c59e8f5912a24625fb3327
CNuge <nugentc@uoguelph.ca>
2018-08-29 15:57:00 -0400

    improved output with a struct

--- revisiting_ctci/RotateMatrix_1_7.go
+++ revisiting_ctci/RotateMatrix_1_7.go
@@ -4,3 +4,81 @@ take an n x n matrix and rotate it by 90 degrees
 can you do it in place?
 
 */
+
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// a structure that is a slice of slices
+// it stores the two dimensional matrix
+type Matrix struct {
+	data [][]int
+}
+
+// represents the 2d matrix with each row on a separate line
+func (m Matrix) String() string {
+	outstring := ""
+	for _, i := range m.data {
+		line_string := fmt.Sprintf("%v\n", i)
+		s := []string{outstring, line_string}
+		outstring = strings.Join(s, "")
+	}
+	return outstring
+}
+
+// add a new row to the matrix
+func (m *Matrix) Fill(row []int) [][]int {
+	m.data = append(m.data, row)
+	return m.data
+}
+
+//rotate the matrix clockwise
+func (m *Matrix) Rotate() [][]int {
+	// initiate a new matrix
+	new_mat := [][]int{}
+	// initiate the necessary # of rows == num cols from original
+	for i := 0; i < len(m.data[0]); i++ {
+		new_mat = append(new_mat, []int{})
+	}
+	// iterate through the reversed i, forward j positions
+	// essentially starting the columns sideways on the left and appending to the rows
+	for i := len(m.data) - 1; i >= 0; i-- {
+		for j, dat := range m.data[i] {
+			new_mat[j] = append(new_mat[j], dat)
+		}
+	}
+	m.data = new_mat
+	return m.data
+}
+
+func main() {
+	// append the rows into the slice
+	row1 := []int{1, 2, 3}
+	row2 := []int{4, 5, 6}
+	row3 := []int{7, 8, 9}
+	row4 := []int{10, 11, 12}
+
+	test_arr1 := Matrix{}
+	test_arr1.Fill(row1)
+	test_arr1.Fill(row2)
+	test_arr1.Fill(row3)
+	test_arr1.Fill(row4)
+
+	fmt.Println(test_arr1)
+
+	test_arr1.Rotate()
+	fmt.Println(test_arr1)
+
+	test_arr2 := Matrix{
+		[][]int{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}}}
+
+	fmt.Println(test_arr2)
+
+	test_arr2.Rotate()
+	fmt.Println(test_arr2)
+
+}
--- revisiting_ctci/ZeroMatrix_1_8.go
+++ revisiting_ctci/ZeroMatrix_1_8.go
@@ -4,3 +4,93 @@ take an m x n matrix and if there is a zero in the matrix then change the entire
 row and column it is in to 0s
 
 */
+package main
+
+import(
+	"fmt"
+	"strings"
+)
+
+type Matrix [][]int
+
+
+// represents the 2d matrix with each row on a separate line
+func (m Matrix) String() string {
+	outstring := ""
+	for _, i := range m {
+		line_string := fmt.Sprintf("%v\n", i)
+		s := []string{outstring, line_string}
+		outstring = strings.Join(s, "")
+	}
+	return outstring
+}
+
+func ZeroMatrix(mat Matrix) Matrix{
+	
+	z_i := []int{}
+	z_j := []int{}
+
+	for i, row := range mat{
+		for j, val := range row{
+			if val == 0{
+				z_i = append(z_i, i)
+				z_j = append(z_j, j)
+			}
+		}
+	}
+
+	for _, i := range z_i{
+		for j:=0; j < len(mat[0]); j++{
+			mat[i][j] = 0
+		}
+	}
+
+	for _, j := range z_j{
+		for i :=0 ; i < len(mat); i ++{
+			mat[i][j] = 0
+		}
+	}
+
+	return mat
+
+}
+
+
+func main(){
+		t1 := Matrix{{0,1,1,},
+					{1,1,1,},
+					{1,0,1,},
+					{1,1,1,},}
+
+
+		o1 := Matrix{{0,0,0,},
+					{0,0,1,},
+					{0,0,0,},
+					{0,0,1,},}
+
+		fmt.Println("start 1:\n")
+		fmt.Println(t1)
+		fmt.Println("post 1:\n")
+		fmt.Println(ZeroMatrix(t1))
+		fmt.Println("compare 1:\n")
+		fmt.Println(o1)
+
+
+		t2 := Matrix{{1,1,1,1,},
+					{1,1,1,1,},
+					{1,1,1,0,},
+					{1,1,1,1,},}
+
+		o2 := Matrix{{1,1,1,0,},
+					{1,1,1,0,},
+					{0,0,0,0,},
+					{1,1,1,0,},}
+
+
+		fmt.Println("start 2:\n")
+		fmt.Println(t2)
+		fmt.Println("post 2:\n")
+		fmt.Println(ZeroMatrix(t2))
+		fmt.Println("compare 2:\n")
+		fmt.Println(o2)
+}
