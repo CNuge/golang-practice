1e64345d29ec81a966092fa58bcfb9a9c28646f9
CNuge <nugentc@uoguelph.ca>
2018-09-04 15:20:54 -0400

    partition nodes around a value

--- revisiting_ctci/Partition_2_4.go
+++ revisiting_ctci/Partition_2_4.go
@@ -20,3 +20,76 @@ whether there are instances where things would be mixed up like
 will work on the assumption this is not the case.
 check when first answer is completed
 */
+
+
+
+package main
+
+import (
+	"fmt"
+	"./linkedlist"
+)
+
+//Remove the node that is currently being viewed from the linked list
+func DelNode(n linkedlist.Node) linkedlist.Node {
+
+	next := *n.Next
+	prev := *n.Prev
+
+	next.Prev = &prev
+	prev.Next = &next
+
+	return next
+}
+
+
+func Partition(ll linkedlist.Node, k int) linkedlist.Node {
+	new_ll := linkedlist.Node{Data: k }
+
+	ll = ll.Front()
+
+	if ll.Data < new_ll.Data{
+		new_ll.FrontAdd(ll.Data)
+	}else{
+		new_ll.Add(ll.Data)
+	}
+	for ll.Next != nil {
+		ll = *ll.Next
+		if ll.Data < new_ll.Data {
+			new_ll.FrontAdd(ll.Data)
+		}else{
+			new_ll.Add(ll.Data)
+		}
+
+	}
+
+	new_ll = DelNode(new_ll)
+	return new_ll
+
+}
+
+
+func main() {
+	data := linkedlist.Node{Data: 4}
+	data.Add(6)
+	data.Add(3)
+	data.FrontAdd(12)
+	data.Add(2)
+	data.Add(2)
+	data.FrontAdd(9)
+	data.Add(11)
+	data.Add(3)
+	data.FrontAdd(1)
+	data.Add(7)
+	data.Add(5)
+
+	fmt.Println(data.Front())
+
+	fmt.Println("Value to partition around: 5")
+
+	data = Partition(data, 5)
+
+	fmt.Println(data.Front())
+
+}
+
