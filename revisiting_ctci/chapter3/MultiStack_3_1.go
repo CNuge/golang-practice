3e8e462dae825a51376ba731384f85903c34f0f5
CNuge <nugentc@uoguelph.ca>
2018-09-10 15:22:35 -0400

    format cleanup

--- revisiting_ctci/chapter3/MultiStack_3_1.go
+++ revisiting_ctci/chapter3/MultiStack_3_1.go
@@ -1,4 +1,3 @@
-
 /*
 use one array to implement three stacks.
 
@@ -6,70 +5,66 @@ i.e.
 
 array := []Stack{3}
 
-have functions to call the array position 
+have functions to call the array position
 and perform the stacks command on the given position
 
 */
 
-
 package main
 
-
-import(
-	"fmt"
+import (
 	"./stack"
+	"fmt"
 )
-
 
 type MultiStack [3]stack.Stack
 
-func (m MultiStack) String() string{
+func (m MultiStack) String() string {
 	outstring := ""
 	for i, dat := range m {
-		outstring = fmt.Sprintf("%v%v\t%v\n",outstring, i, dat )
+		outstring = fmt.Sprintf("%v%v\t%v\n", outstring, i, dat)
 	}
 	return outstring
 }
 
-func (m *MultiStack) Push( dat, pos int){
+func (m *MultiStack) Push(dat, pos int) {
 	m[pos].Push(dat)
 }
 
 func (m *MultiStack) Pop(pos int) int {
-	output :=  m[pos].Pop()
+	output := m[pos].Pop()
 	return output
 }
 
-func (m MultiStack) IsEmpty(pos int) bool{
+func (m MultiStack) IsEmpty(pos int) bool {
 	if len(m[pos].Ord) == 0 {
 		return true
 	}
 	return false
 }
 
-
-func main(){
+func main() {
 
 	test := MultiStack{}
 	fmt.Println(test.IsEmpty(0))
-	test.Push(1,0)
-	test.Push(2,0)
-	test.Push(3,0)
-	test.Push(4,0)
+	test.Push(1, 0)
+	test.Push(2, 0)
+	test.Push(3, 0)
+	test.Push(4, 0)
 	fmt.Println(test)
 
 	fmt.Println(test.IsEmpty(0))
 	test.Pop(0)
 	fmt.Println(test)
 
-	test.Push(4,1)
-	test.Push(5,1)
-	test.Push(6,1)
+	test.Push(4, 1)
+	test.Push(5, 1)
+	test.Push(6, 1)
 
-	test.Push(7,2)
-	test.Push(8,2)
-	test.Push(9,2)
-	
+	test.Push(7, 2)
+	test.Push(8, 2)
+	test.Push(9, 2)
+
 	fmt.Println(test)
-	
+
 }
