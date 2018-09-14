1a92202fcc509b4e7822eae758339a3c7c4817e7
Cam Nugent <Cam@Minerva.local>
2018-09-14 14:29:11 -0400

    python version up and running

--- revisiting_ctci/chapter3/TwoQueueStructs_3_6.py
+++ revisiting_ctci/chapter3/TwoQueueStructs_3_6.py
@@ -13,36 +13,64 @@ implement this setup
 
 """
 
+from queue import Queue
 
 
-class Queue():
+class Shelter():
 	def __init__(self):
-		self.__dat = []
+		self.Cats = Queue()
+		self.Dogs = Queue()
+		self.id = 0
 
-	def Push(self, n):
-		""" add a number to the top of the stack """
-		self.__dat.append(n)
+	def Add(self, animal, species):
+		"""take a cat or dog and its name, add to the shelter"""
+		self.id +=1
+		if species == 'Cat':
+			self.Cats.Push((animal, species , self.id))  
+		elif species == 'Dog':
+			self.Dogs.Push((animal, species , self.id))
+		else:
+			raise ValueError('Must specify the species of the animal. String: Cat or Dog')
 
-	def Peek(self):
-		""" look at the number at the top of the stack """
-		return self.__dat[0]
+	def Adopt(self, preference = None):
+		""" if preference given, adopt oldest member of that queue, 
+			otherwise compare data of the two and adopt lower id number """
+		if preference == 'Cat':
+			return self.Cats.Pop()
+		elif preference == 'Dog':
+			return self.Dogs.Pop()
+		elif preference == None:
+			cat_last = self.Cats.Peek()
+			dog_last = self.Dogs.Peek()
+			if cat_last[-1] < dog_last [-1]:
+				return self.Cats.Pop()
+			else:
+				return self.Dogs.Pop()
 
-	def Pop(self):
-		return self.__dat.pop(0)
+		else:
+			raise ValueError('Must specify the preferred species of the animal. String: Cat or Dog\n'+\
+								'If no preference then no value should be passed.')
 
-	def IsEmpty(self):
-		if len(self.__dat) == 0:
-			return True
-		return False
 
-	@property
-	def Data(self):
-		return [x for x in self.__dat]
+if __name__ == '__main__':
 
-	def __repr__(self):
-		outstring = ''
-		""" show the data in the stack """
-		for i in reversed(self.__dat):
-			outstring += str(i)
-			outstring += '\t'
-		return outstring
+	test_shelter = Shelter()
+
+	test_shelter.Add('Lassie', 'Dog')
+
+	test_shelter.Add('Garfield', 'Cat')
+	test_shelter.Add('Andrew', 'Cat')
+	test_shelter.Add('Lloyd', 'Cat')
+	test_shelter.Add('Weber', 'Cat')
+
+	test_shelter.Add('Mr. Pickles', 'Dog')
+
+	test_shelter.Add('Alfie', 'Dog')
+
+	print(test_shelter.Adopt('Cat'))
+	print(test_shelter.Adopt())
+	print(test_shelter.Adopt('Cat'))
+	print(test_shelter.Adopt('Dog'))
+	print(test_shelter.Adopt())
+	print(test_shelter.Adopt('Dog'))
+
