0c25c4665dfef5d7f30ebd3af2c35809b738ab3f
CNuge <nugentc@uoguelph.ca>
2018-09-18 14:52:05 -0400

    route between nodes

--- revisiting_ctci/chapter4/RouteBetweenNodes_4_1.py
+++ revisiting_ctci/chapter4/RouteBetweenNodes_4_1.py
@@ -3,12 +3,26 @@ Given a directed graph, design an algorithm to find out whther there is a
 route between two nodes
 """
 
+from graph import Node, Graph
+
+
+def IsRoute(TopNode, dest):
+	""" conduct a breadth first search to see if there is a path between
+		the start node and the destination"""
+	nodes = [TopNode]
+	while len(nodes) > 0 :
+		current = nodes.pop(0)
+		if current.Data == dest:
+			return True
+		nodes.extend(current.adj)
+
+	return False
+
 
 #conduct a breadth first search, beginning with a seed node and visiting
 #all of its progeny, and their progeny in turn.
 
 
-#testing if github working again
 
 
 if __name__ == '__main__':
