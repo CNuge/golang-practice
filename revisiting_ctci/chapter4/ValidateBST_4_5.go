481c95d13a9bf8777df396735211a6a463c6de55
CNuge <nugentc@uoguelph.ca>
2018-10-01 21:37:54 -0400

    first try at validating bst

--- revisiting_ctci/chapter4/ValidateBST_4_5.go
+++ revisiting_ctci/chapter4/ValidateBST_4_5.go
@@ -11,9 +11,39 @@ import(
 
 
 
-func ValidateBST(s graph.BNode) bool{
+func ValidateBST(s graph.BNode) bool {
 
+	var left_ans bool
+	var right_ans bool
 
+	if s.Left == nil{
+		left_ans = true
+	} else {
+
+		left := *s.Left
+		if left.Data > s.Data{
+			return false
+		}
+
+		left_ans = ValidateBST(left)
+	}
+
+	if s.Right == nil {
+		right_ans = true	
+	} else{
+		right := *s.Right	
+		if right.Data < s.Data{
+			return false
+		}
+
+		right_ans = ValidateBST(right)		
+	}
+
+	if right_ans == true && left_ans == true {
+		return true
+	}
+
+	return false
 }
 
 
@@ -21,7 +51,16 @@ func main(){
 
 	sorted_array := []int{1,3,5,7,9,11,13}
 
-	test_tree := graph.BuildBalancedTree(sorted_array)
+	test_tree_1 := graph.BuildBalancedTree(sorted_array)
 
+	fmt.Println(sorted_array)
+	fmt.Println(ValidateBST(test_tree_1))
+
+	non_sorted_array := []int{1,7,9,11,3,5,13}
+
+	test_tree_2 := graph.BuildBalancedTree(non_sorted_array)
+
+	fmt.Println(non_sorted_array)
+	fmt.Println(ValidateBST(test_tree_2))
 
 }
